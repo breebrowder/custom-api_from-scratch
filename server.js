7e55a7eb47c32735a89171bd7fb9c24a37c8b69a
breezybrow <browder.bree@gmail.com>
2022-02-24 06:47:51 +0000

    push all contents to github

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const express = require('express')
 const app = express()
 //  create variable that pulls in mongoose library
 const mongoose = require('mongoose')
+const User = require('./users')
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 // on error log that there is an error if problem connecting to database
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
@@ -16,10 +17,60 @@ db.once('open', () => console.log('Connected to Database'))
 // setup server to use json
 app.use(express.json())
 
-const subscribersRouter = require('./routes/subscribers')
-app.use('/subscribers', subscribersRouter)
+db.once('open', async () => {
+  if (await User.countDocuments().exec() > 0) return
 
+  Promise.all([
+    User.create({ name: 'User 1' }),
+    User.create({ name: 'User 2' }),
+    User.create({ name: 'User 3' }),
+    User.create({ name: 'User 4' }),
+    User.create({ name: 'User 5' }),
+    User.create({ name: 'User 6' }),
+    User.create({ name: 'User 7' }),
+    User.create({ name: 'User 8' }),
+    User.create({ name: 'User 9' }),
+    User.create({ name: 'User 10' }),
+    User.create({ name: 'User 11' }),
+    User.create({ name: 'User 12' })
+  ]).then(() => console.log('Added Users'))
+})
 
-// URL is 'localhost:3000/cars'. Anything with this url or anything like 'localhost:3000/cars/anything' goes into the sportsCarRouter
+app.get('/users', paginatedResults(User), (req, res) => {
+  res.json(res.paginatedResults)
+})
+
+function paginatedResults(model) {
+  return async (req, res, next) => {
+    const page = parseInt(req.query.page)
+    const limit = parseInt(req.query.limit)
+
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+
+    const results = {}
+
+    if (endIndex < await model.countDocuments().exec()) {
+      results.next = {
+        page: page + 1,
+        limit: limit
+      }
+    }
+    
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit
+      }
+    }
+    try {
+      results.results = await model.find().limit(limit).skip(startIndex).exec()
+      res.paginatedResults = results
+      next()
+    } catch (e) {
+      res.status(500).json({ message: e.message })
+    }
+  }
+}
 
 app.listen(3000, () => console.log('The server has started!'))
